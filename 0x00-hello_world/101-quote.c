062b70301ea437572b08039bfbebe61092c99fae
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-08 04:46:45 -0800

    101-quote.c corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"
-		       	- Dora Korpar, 2015-10-19\n", 58);
+	fprintf(stderr, "and that piece of art is useful\"
+		       	- Dora Korpar, 2015-10-19\n");
 	return (1);
 }
