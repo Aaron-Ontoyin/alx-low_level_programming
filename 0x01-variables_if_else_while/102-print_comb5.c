66309b98cb013f957971084b07159686545e9761
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-09 11:18:48 -0800

    Added 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point of program
+ *
+ * Return: Returns 0 for success
+ */
+int main(void)
+{
+	int a,b,c,d;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b < '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+					if (b > c)
+						continue;
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					putchar(',');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
