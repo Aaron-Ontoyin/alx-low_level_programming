8c321e3e3b2f615f8c20be89d70a43c70fc12538
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-09 09:03:47 -0800

    Added 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point of program
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	for (l = 'A'; l <= 'Z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
