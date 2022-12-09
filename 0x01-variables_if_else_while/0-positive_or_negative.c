2022e9d54b0cd9f28e064c2f377775145911fccd
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-09 07:12:07 -0800

    0-positive or negative corrected

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positve\n");
+		printf("%d is positve\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n");
+		printf("%d is negative\n", n);
 	}
 	else
 	{
