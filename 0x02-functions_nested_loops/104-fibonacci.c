db36c2c16a77c44f398c71ed4f4ebe5942221db6
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-13 15:41:44 -0800

    Corrected 104-fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,17 +10,42 @@ int main(void)
 	long temp;
 	long p1 = 0;
 	long p2 = 1;
+	long p1_half1, p1_half2, p2_half1, p2_half2;
+	long half1, half2;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 92; i++)
 	{
 		printf("%lu", p1 + p2);
 		temp = p1;
 		p1 = p2;
 		p2 = temp + p2;
-		if (i < 97)
-			printf(", ");
+		printf(", ");
 	}
 
+	p1_half1 = p1 / 10000000000;
+	p2_half1 = p2 / 10000000000;
+	p1_half2 = p1 % 10000000000;
+	p2_half2 = p2 % 10000000000;
+
+	for (i = 92; i < 98; i++)
+	{
+		half1 = p1_half1 + p2_half1;
+		half2 = p1_half2 + p2_half2;
+		if (p1_half2 + p2_half2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+
+		printf("%lu%lu", half1, half2);
+		if (i < 98)
+			printf(", ");
+
+		p1_half1 = p2_half1;
+		p1_half2 = p2_half2;
+		p2_half1 = half1;
+		p2_half2 = half2;
+	}
 	printf("\n");
 
 	return (0);
