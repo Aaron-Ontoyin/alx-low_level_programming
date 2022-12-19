e51e33a9ef05e186672d7c9475e63f79c0ebb975
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-19 13:21:56 -0800

    added 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+/**
+ * main - Generates random password valid password for 102-crack me
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, c, j = 0;
+	srand(time(NULL));
+
+	/* my wicked password */
+	for (i = 0; i < 10; i++)
+	{
+		while (j < 10000)
+		{
+			c = rand() % 128;
+			j  += c;
+			write(1, &c, 1);
+		}
+		j = 0;
+	}
+
+	return (0);
+}
