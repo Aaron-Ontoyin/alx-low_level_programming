795b6e2f05a2ffe9c903bfb99b4b611d2dae6ef4
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-19 05:59:37 -0800

    Corrected 6-puts2 so i wont overpass len of string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- * puts2 - prints a string to at a step of 2 starting from 1st char
+ * puts2 - prints a string at a step of 2 starting from 1st char
  * @str: string to be printed
  */
 void puts2(char *str)
 {
 	int i = 0;
+	int len = 0;
 
-	while (str[i] != '\0')
+	while (str[len] != '\0')
+		len++;
+
+	len--; /* indexing len */
+	while (i <= len)
 	{
 		_putchar(str[i]);
 		i += 2;
