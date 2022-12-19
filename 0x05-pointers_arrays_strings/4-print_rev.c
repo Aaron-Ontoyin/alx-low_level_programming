f399a5b3eea5857298db32725456534fd11564b7
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-19 05:19:20 -0800

    Added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to string
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
