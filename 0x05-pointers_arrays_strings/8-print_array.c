faaadbd9e716570695aca23e937246be0a11cabe
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-19 06:26:43 -0800

    Added 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints first n elements of an array of
+ * integers sep by commas, ended by a new line
+ * @a: array
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int idx;
+
+	for (idx = 0; idx < n - 1; idx++)
+	{
+		printf("%d, ", *(a + idx));
+	}
+	printf("%d\n", *(a + n - 1));
+}
