2c30f3eafce29b16feb98fd5df254fe479d25e63
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-19 13:45:18 -0800

    modeified algo

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,20 +9,18 @@
  */
 int main(void)
 {
-	int i, c, j = 0;
+	int i = 0, c;
 	srand(time(NULL));
 
-	/* my wicked password */
-	for (i = 0; i < 10; i++)
+	while (i <= 2646)
 	{
-		while (j < 10000)
-		{
-			c = (rand() % 90) + 37;
-			j  += c;
-			write(1, &c, 1);
-		}
-		j = 0;
+		c = rand() % 128;
+		write(1, &c, 1);
+		i += c;
 	}
+
+	c = 2772 - i;
+	write(1, &c, 1);
 
 	return (0);
 }
