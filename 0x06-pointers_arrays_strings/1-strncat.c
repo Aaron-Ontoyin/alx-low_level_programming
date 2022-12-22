8ab0955ea361ca9c5d51c2a8c7490fcfe93c39db
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-22 02:31:26 -0800

    Added strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - Adds n bytes of first str to the second
+ * @dest: first string
+ * @src: second string
+ * @n: number of bytes to add
+ * Return: pointer to second string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	while (dest[dest_len] !=  '\0')
+		dest_len++;
+
+	while (i < n)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
