eb8456978a522abce57771f49ce49375aa92d76e
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-22 06:30:33 -0800

    Added 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - Converts entrire string to upper case
+ * @s: pointer to string
+ * Return: pointer to string
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
