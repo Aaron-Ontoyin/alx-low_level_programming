29434e5ea4cb4f4e2547f63d76828b7794250938
Aaron-Ontoyin <aarononto909@gmail.com>
2022-12-22 07:10:17 -0800

    changed tabs to space

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,12 +14,17 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
+		k = i + 1;
+
 		for (j = 0; j < 13; j++)
 		{
-			k = i + 1;
 			if (s[i] == sep[j] && (s[k] >= 'a' && s[k] <= 'z'))
 				s[i + 1] = s[i + 1] - 32;
 		}
+
+		if  (s[i] == '\t')
+			s[i] = ' ';
+	
 		i++;
 	}
 	return (s);
