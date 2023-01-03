8ae5d2d6ccfca535f1d723b3aaa919a20ded16d7
Aaron-Ontoyin <aarononto909@gmail.com>
2023-01-03 12:44:30 -0800

    Added memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills first n bytes of mem pointed to by s with const byte b
+ * @s: pointer to memory to be filled
+ * @b: const byte to be used to fill
+ * @n: number of bytes to fill
+ * Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
