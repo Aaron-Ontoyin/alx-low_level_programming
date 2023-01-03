d236280404376ce5c8fbbfb5b42bd04a16a3a234
Aaron-Ontoyin <aarononto909@gmail.com>
2023-01-03 14:39:44 -0800

    Addeed 5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - finds first occurence of substring needle in haystaaack
+ * @needle: substring searched for
+ * @haystack: string searched in
+ * Return: pointer to beginning of substring or NULL for unfound case
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int j = 0;
+
+	while (*haystack)
+	{
+		if (*haystack == needle[0])
+		{
+			while (1)
+			{
+				if (needle[j] == '\0')
+					return (haystack);
+
+				if (needle[j] != *(haystack + j))
+					break;
+
+				j++;
+			}
+		}
+
+		haystack++;
+	}
+
+	return (NULL);
+}
