474e23afdcee2f1a928b44385abaad0eea96be57
Aaron-Ontoyin <aarononto909@gmail.com>
2023-01-05 16:19:53 -0800

    Added 2-strlen_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse followed by a new line
+ * _print_rev_recursion - prints a string in reverse
  * @s: pointer to the string
  */
 
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to the string
+ * Return: integer repping the length of s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recursion(++s));
+
+}
