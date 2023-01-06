bc1a9feec101b599937b168d616f6de1461d9ee6
Aaron-Ontoyin <aarononto909@gmail.com>
2023-01-05 16:11:17 -0800

    Added 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse followed by a new line
+ * @s: pointer to the string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+
+	_putchar(s[0]);
+
+}
