eb7d43aefc896df46141c86c27076bedf7edea73
Aaron-Ontoyin <aarononto909@gmail.com>
2023-01-09 07:34:12 -0800

    Considered neg numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,23 +11,32 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
+	/*to avoid unused argc*/
+	if (argc)
 
-	if (argc > 1)
+	while (*(++argv))
 	{
-		while (*(++argv))
+		if (**argv == '-')
 		{
-			if (isdigit(**argv))
+			if (isdigit(*(*argv + 1)))
 			{
-				sum += atoi(*argv);
-			} else
+				sum -= atoi(*argv + 1);
+			}
+			else
 			{
 				printf("Error\n");
 				return (1);
 			}
+		} else if (isdigit(**argv))
+		{
+			sum += atoi(*argv);
+		} else
+		{
+			printf("Error\n");
+			return (1);
 		}
-
 	}
-	printf("%d\n", sum);
 
+	printf("%d\n", sum);
 	return (0);
 }
