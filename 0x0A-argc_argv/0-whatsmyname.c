32706fddaacbf72cf4c715286bb57d55530a7aa9
Aaron-Ontoyin <aarononto909@gmail.com>
2023-01-09 06:02:54 -0800

    Added 0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the program runnig it
+ * @argc: number of cmdline args passed to it
+ * @argv: an array of pointers to string args passed to it
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
