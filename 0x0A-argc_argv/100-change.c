145721f7a9f28a907eaa13c0ff204f9b04f01e51
Aaron-Ontoyin <aarononto909@gmail.com>
2023-01-09 08:54:01 -0800

    Added 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+/**
+ * cal_num_coins - calculates the num of coins given the total val
+ * @v: total val of coins
+ * Return: number of possible minimum coins given their total is v
+ */
+int cal_num_coins(int v)
+{
+	int n = 0;
+
+	while (v != 0)
+	{
+		if (v % 10 == 9 || v % 10 == 7)
+			v -= 2;
+		else if (v % 25 == 0)
+			v -= 25;
+		else if (v % 10 == 0)
+			v -= 10;
+		else if (v % 5 == 0)
+			v -= 5;
+		else if (v % 2 == 0)
+		{
+			if (v % 10 == 6)
+				v -= 1;
+			else
+				v -= 2;
+		}
+		else
+			v -= 1;
+
+		n++;
+	}
+
+	return (n);
+}
+
+/**
+ * main - finds the minimum number of coins as change
+ * @argc: number of cmdline args passed to it
+ * @argv: an array of pointers to string args passed to it
+ * Return: 0 for success; 1 for error if argc is not 2 or argv[1] < 0
+ */
+int main(int argc, char *argv[])
+{
+	int change;
+	int num_coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	change = atoi(argv[1]);
+
+	if (change >= 0)
+		num_coins = cal_num_coins(change);
+
+	printf("%d\n", num_coins);
+	return (0);
+}
+
