ac3d8de29f3657072022777c196b047f4afe3e9a
Aaron-Ontoyin <aarononto909@gmail.com>
2023-01-10 03:48:15 -0800

    Added 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * _strdup - copies a string to a new address
+ * @str:pointer to the string
+ * Return: pointer to new address copied to; NULL if failed or *str is NULL
+ */
+char *_strdup(char *str)
+{
+	int s = 0;
+	int i;
+	char *new_adrr;
+
+	while (str[s] != '\0')
+		s++;
+
+	new_adrr = (char *) malloc(sizeof(char) * s);
+
+	if (new_adrr == NULL)
+		return (NULL);
+
+	for (i = 0; i < s; i++)
+		new_adrr[i] = str[i];
+
+	return (new_adrr);
+}
