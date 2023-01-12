f95685eb981eca94ceed3e30afcfe01347525f82
Aaron-Ontoyin <aarononto909@gmail.com>
2023-01-12 14:19:36 -0800

    compied minimum of new_size and old_size of content to new mem

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,7 +23,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *nptr;
 	unsigned char *temp_ptr = ptr;
-	unsigned int n = (old_size > new_size ? old_size : new_size);
+	unsigned int n = (old_size > new_size ? new_size : old_size);
 	unsigned int i;
 
 	if (new_size == old_size)
