7f4d341f991f2a937a51c4fa0f12fa55839a61d9
Aaron-Ontoyin <aarononto909@gmail.com>
2023-03-20 15:53:43 -0700

    changed git path

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+wget -O /tmp/win.so https://github.com/Aaron-Ontoyin/alx-low_level_programming/blob/3ba0105be190790d69213c41a1f0143ed9731285/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
 
